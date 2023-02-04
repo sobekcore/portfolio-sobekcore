44f4bf31a2c5cd941bd0e9928ecd2d526e8aa613
David <sobekcore@gmail.com>
2023-02-04 14:33:31 +0100

    Re-implement ESLint and Prettier to keep code style consistent

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+  ],
+};
