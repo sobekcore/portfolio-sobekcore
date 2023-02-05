95e5af6bba60398748c4dcc439c2ccb49319efeb
David <sobekcore@gmail.com>
2023-02-05 18:53:37 +0100

    Design and implement a new Launchpad home page for this project

---
+++ src/types/props.ts
@@ -0,0 +1,2 @@
+export type PropsClass = string | object;
+export type PropsStyle = string | object;
--- src/types/swup.d.ts
+++ src/types/swup.d.ts
@@ -1,3 +1,4 @@
 declare module '@swup/slide-theme';
 declare module '@swup/head-plugin';
+declare module '@swup/scripts-plugin';
 declare module '@swup/a11y-plugin';
